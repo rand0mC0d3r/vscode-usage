44aab9a10673410904a347f5ca2c8e69853fd89e
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-02-26 02:48:18 +0100

    updated UI

--- npm-component-usage/extension.js
+++ npm-component-usage/extension.js
@@ -1,10 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const fs = require('fs');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -12,23 +7,11 @@ const fs = require('fs');
 function activate(context) {
 
 	let subscriptions = [];
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "npm-component-usage" is now active!');
 
-
-	// // The command has been defined in the package.json file
-	// // Now provide the implementation of the command with  registerCommand
-	// // The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('npm-component-usage.helloWorld', function () {
-	// 	// The code you place here will be executed every time your command is executed
-
-	// 	const activeDocumentTitle = vscode.window.activeTextEditor.document.getText()
-	// 	if (['.js', '.jsx', '.ts', '.tsx'].some(i => activeDocumentTitle.includes(i))) {
-	// 		vscode.window.showInformationMessage(`Active file: ${activeDocumentTitle}`);
-	// 	}
-	// });
-
+	const	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = 'npm-component-usage.open';
+	subscriptions.push(myStatusBarItem);
 
 	vscode.window.onDidChangeActiveTextEditor((editor) => {
 		const activeDocumentTitle = vscode.window.activeTextEditor.document.fileName.split('/')
@@ -39,20 +22,20 @@ function activate(context) {
 		if (['.js', '.jsx', '.ts', '.tsx'].some(i => fileName.includes(i))) {
 			let filesFound = []
 			// vscode.window.showInformationMessage(`Active folder: ${folderName} file: ${fileName}`);
-
-			vscode.workspace.findFiles('src/**/*').then(res => {
+			const targetFolder = vscode.workspace.getConfiguration('myExtension').get('includedFolders')
+			myStatusBarItem.text = `Searching...`;
+			myStatusBarItem.show();
+			vscode.workspace.findFiles(targetFolder || 'src/**/*').then(res => {
 			res
 				.filter(resItem => !resItem.path.includes('node_modules'))
 				.map(resItem => {
 					const content = fs.readFileSync(resItem.path, 'utf8')
 					if (fileName.includes('index')) {
 						if (content.includes(`/${folderName}'`)) {
-							// console.log(resItem.path)
 							filesFound.push(resItem.path)
 						}
 					} else {
 						if (content.includes(`/${fileName}'`)) {
-							// console.log(resItem.path)
 							filesFound.push(resItem.path)
 						}
 					}
@@ -63,20 +46,14 @@ function activate(context) {
 				} else {
 					vscode.window.showWarningMessage('No files found');
 				}
-
-		})
+			})
+			myStatusBarItem.hide();
 		}
-
-
-
 	}, null, subscriptions);
 
-
 	context.subscriptions.push(vscode.Disposable.from(...subscriptions));
-	// context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {

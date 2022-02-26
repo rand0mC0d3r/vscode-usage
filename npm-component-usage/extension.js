a424a24dc4761cffb27e87fd609781f3ddc1abb1
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-02-26 01:22:49 +0100

    updated UI

--- npm-component-usage/extension.js
+++ npm-component-usage/extension.js
@@ -35,23 +35,39 @@ function activate(context) {
 		const fileName = activeDocumentTitle[activeDocumentTitle.length - 1]
 		const folderName = activeDocumentTitle[activeDocumentTitle.length - 2]
 
-		if (['.js', '.jsx', '.ts', '.tsx'].some(i => fileName.includes(i))) {
-			vscode.window.showInformationMessage(`Active folder: ${folderName} file: ${fileName}`);
-		}
 
-		vscode.workspace.findFiles('src/**/*').then(res => {
+		if (['.js', '.jsx', '.ts', '.tsx'].some(i => fileName.includes(i))) {
+			let filesFound = []
+			// vscode.window.showInformationMessage(`Active folder: ${folderName} file: ${fileName}`);
+
+			vscode.workspace.findFiles('src/**/*').then(res => {
 			res
 				.filter(resItem => !resItem.path.includes('node_modules'))
 				.map(resItem => {
 					const content = fs.readFileSync(resItem.path, 'utf8')
 					if (fileName.includes('index')) {
-						content.includes(`/${folderName}'`) && console.log(resItem.path)
+						if (content.includes(`/${folderName}'`)) {
+							// console.log(resItem.path)
+							filesFound.push(resItem.path)
+						}
 					} else {
-						content.includes(`/${fileName}'`) && console.log(resItem.path)
+						if (content.includes(`/${fileName}'`)) {
+							// console.log(resItem.path)
+							filesFound.push(resItem.path)
+						}
 					}
-					// console.log(resItem.path)
 				})
+				// console.log(filesFound)
+				if (filesFound.length > 0) {
+					vscode.window.showInformationMessage(`${filesFound.length} files found: ${filesFound.join(', ')}`);
+				} else {
+					vscode.window.showWarningMessage('No files found');
+				}
+
 		})
+		}
+
+
 
 	}, null, subscriptions);
 
